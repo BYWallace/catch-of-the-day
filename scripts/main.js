f5fb5ca61a72bd89afb6b5fab9e7c2b393dbabc1
Brett Wallace <bwallace90@gmail.com>
2016-05-28 15:03:42 -0400

    Lay out app components

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,54 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-/* StorePicker component */
+/* App */
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+});
+
+/*  Header */
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+/*  Order */
+
+var Order = React.createClass({
+  render: function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*  Inventory */
+
+var Inventory = React.createClass({
+  render: function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
+/* StorePicker */
 
 var StorePicker = React.createClass({
   render: function() {
@@ -16,4 +63,4 @@ var StorePicker = React.createClass({
 
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'))
+ReactDOM.render(<App/>, document.querySelector('#main'))
