f28c9047882bcde7094610f1635f175d569a7f84
Brett Wallace <bwallace90@gmail.com>
2016-05-28 14:41:57 -0400

    Add React and create first StorePicker component

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/* StorePicker component */
+
+var StorePicker = React.createClass({
+  render: function() {
+    return (
+      <p>hello</p>
+    )
+  }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'))
