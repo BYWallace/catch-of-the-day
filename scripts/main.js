2034c2a05d9066f0281fe8895f0f178730532305
Brett Wallace <bwallace90@gmail.com>
2016-07-06 17:06:18 -0400

    Set up syncing with Firebase

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,11 @@ var History = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers');
+
+// Firebase
+var Rebase = require('re-base');
+var base = Rebase.createClass('https://catch-of-the-day-b1a88.firebaseio.com');
+
 /* App */
 
 var App = React.createClass({
@@ -18,6 +23,12 @@ var App = React.createClass({
       fishes: {},
       order: {}
     }
+  },
+  componentDidMount: function() {
+    base.syncState(this.props.params.storeId + '/fishes', {
+      context: this,
+      state: 'fishes'
+    });
   },
   addToOrder: function(key) {
     this.state.order[key] = this.state.order[key] + 1 || 1;
