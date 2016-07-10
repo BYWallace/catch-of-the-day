9553738e1dee47cf26caf9ba15054d95bd35a0a2
Brett Wallace <bwallace90@gmail.com>
2016-07-10 15:34:12 -0400

    Refactor Header to ES6 class with propTypes

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -1,23 +1,24 @@
 /*  Header */
 import React from 'react';
 
-var Header = React.createClass({
-  render: function() {
+class Header extends React.Component {
+  render() {
     return (
       <header className="top">
-        <h1>Catch
-          <span className="ofThe">
-            <span className="of">of</span>
-            <span className="the">the</span>
-          </span>
-          Day</h1>
-        <h3><span>{this.props.tagline}</span></h3>
+      <h1>Catch
+      <span className="ofThe">
+      <span className="of">of</span>
+      <span className="the">the</span>
+      </span>
+      Day</h1>
+      <h3><span>{this.props.tagline}</span></h3>
       </header>
     )
-  },
-  propTypes: {
-    tagline: React.PropTypes.string.isRequired
   }
-});
+}
+
+Header.propTypes = {
+  tagline: React.PropTypes.string.isRequired
+}
 
 export default Header;
